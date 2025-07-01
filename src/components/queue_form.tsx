b7cfbb2352b2055c7ef26c0eafcba484535e94af
Guillaume RICARD <guillaume.ricard@mail.com>
2025-07-01 17:55:04 +0900

    home: Perform basic form validation
    Update disabled status and validation error messages on user input.

--- src/components/queue_form.tsx
+++ src/components/queue_form.tsx
@@ -1,12 +1,57 @@
+import { useState, useCallback, useEffect, useMemo } from "react";
+
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
 import { NumberField } from "@base-ui-components/react/number-field";
 
+import {
+  SEAT_CAPACITY,
+  MIN_PARTY_SIZE,
+  DEFAULT_PARTY_SIZE,
+  type QueueItemData,
+} from "../lib/constants.ts";
+
 import styles from "./queue_form.module.css";
 
-export default function QueueForm() {
+export default function QueueForm({
+  onSubmit,
+}: {
+  onSubmit?: (queueItem: QueueItemData) => void;
+}) {
+  const [name, setName] = useState("");
+  const [partySize, setPartySize] = useState<number>(DEFAULT_PARTY_SIZE);
+
+  const disabled = useMemo(() => name === "", [name]);
+
+  // Warn on empty name
+  const [inputIsError, setInputIsError] = useState(false);
+  useEffect(() => {
+    setInputIsError(disabled);
+  }, [name]);
+  // Don't show error before edits
+  useEffect(() => {
+    setInputIsError(false);
+  }, []);
+
+  // Load saved form values from local storage.
+  useEffect(() => {
+    const storedName = localStorage.getItem("pp-name");
+    if (storedName !== null) {
+      setName(storedName);
+    }
+  });
+
+  const validateAndSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    if (onSubmit) {
+      onSubmit({ name, partySize });
+    }
+  }, [name, partySize]);
+
   return (
     <>
       <Typography variant="h2">Hello</Typography>
@@ -14,13 +59,24 @@ export default function QueueForm() {
         Welcome to Waitlist Manager, please input your details to join the
         queue.
       </Typography>
-      <TextField label="Name" variant="standard" />
+      <TextField
+        required
+        error={inputIsError}
+        helperText={inputIsError ? "Required" : ""}
+        id="name"
+        label="Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        onBlur={() => setInputIsError(disabled)}
+      />
       <NumberField.Root
         id="party-size"
-        defaultValue={2}
-        min={1}
-        max={10}
+        defaultValue={DEFAULT_PARTY_SIZE}
+        min={MIN_PARTY_SIZE}
+        max={SEAT_CAPACITY}
         className={styles.Field}
+        value={partySize}
+        onValueChange={(value) => setPartySize(value ?? DEFAULT_PARTY_SIZE)}
       >
         <NumberField.ScrubArea className={styles.ScrubArea}>
           <label htmlFor="toto" className={styles.Label}>
@@ -37,7 +93,11 @@ export default function QueueForm() {
           </NumberField.Increment>
         </NumberField.Group>
       </NumberField.Root>
-      <Button variant="contained" onClick={() => alert("ok")}>
+      <Button
+        variant="contained"
+        onClick={validateAndSubmit}
+        disabled={disabled}
+      >
         Join
       </Button>
     </>
---
+++ src/lib/constants.ts
@@ -0,0 +1,27 @@
+/**
+ * @file Waitlist Manager useful constants
+ */
+
+/**
+ * Maximum restaurant seat capacity.
+ *
+ * Hardcoded to ten seats.
+ */
+export const SEAT_CAPACITY = 10;
+
+/**
+ * Allow single diners to join the queue.
+ */
+export const MIN_PARTY_SIZE = 1;
+
+/**
+ * Assume party size is most likely two.
+ *
+ * Default party size input value if not saved in local storage.
+ */
+export const DEFAULT_PARTY_SIZE = 2;
+
+export type QueueItemData = {
+  name: string;
+  partySize: number;
+};
