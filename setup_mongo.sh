50619227a10b3d592c4f88b12b5374a099c753e9
Guillaume RICARD <guillaume.ricard@mail.com>
2025-07-02 13:10:40 +0900

    docker: Setup Mongo replica set
    Enable transaction support on docker-managed mongo database backend.

---
+++ setup_mongo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Wait for MongoDB to be up
+until mongosh --host mongodb:27017 --eval 'quit(db.runCommand({ping: 1}).ok ? 0 : 2)' &>/dev/null; do
+  printf '.'
+  sleep 1
+done
+
+# Setup MongoDB Replica Set
+cd /
+echo 'rs.initiate({"_id": "rs0", "version": 1, "members": [{"_id": 0, "host": "mongodb:27017", "priority": 2}]})' > config-replica.js
+mongosh -u "$DATABASE_USER" -p "$DATABASE_PASSWORD" --host mongodb:27017 /config-replica.js
