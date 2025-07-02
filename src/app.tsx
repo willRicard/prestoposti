f8076c350f069dfcf6356917b9d81cc93120b4a5
Guillaume RICARD <guillaume.ricard@mail.com>
2025-07-02 22:10:28 +0900

    home: Move container UI to router definition
    Hoist app root layout to router configuration.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 
 import Alert from "@mui/material/Alert";
-import Container from "@mui/material/Container";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
 import Snackbar from "@mui/material/Snackbar";
 
 import QueueForm from "./components/queue_form";
@@ -67,31 +64,18 @@ const App = () => {
   };
 
   return (
-    <Container>
-      <div className="absolute inset-0 flex flex-col items-center justify-center gap-9">
-        <QueueForm onSubmit={handleSubmit} />
-        <Snackbar
-          open={error !== ""}
-          autoHideDuration={MODAL_DELAY}
-          onClose={handleErrorClose}
-        >
-          <Alert severity="error" onClose={handleErrorClose}>
-            Failed joining the queue. Please try again.
-          </Alert>
-        </Snackbar>
-      </div>
-      <footer className="py-3 absolute bottom-0 left-0 right-0 flex items-center justify-between">
-        <div></div>
-        <span>&copy; 2025 Guillaume RICARD. All rights reserved.</span>
-        <div className="mr-5">
-          Language select:&nbsp;
-          <Select defaultValue="en">
-            <MenuItem value="en">English</MenuItem>
-            <MenuItem value="ja">日本語</MenuItem>
-          </Select>
-        </div>
-      </footer>
-    </Container>
+    <>
+      <QueueForm onSubmit={handleSubmit} />
+      <Snackbar
+        open={error !== ""}
+        autoHideDuration={MODAL_DELAY}
+        onClose={handleErrorClose}
+      >
+        <Alert severity="error" onClose={handleErrorClose}>
+          Failed joining the queue. Please try again.
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,32 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
   return (
-    <>
-      <Outlet />
+    <Container>
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-9">
+        <Outlet />
+      </div>
+      <footer className="py-3 absolute bottom-0 left-0 right-0 flex items-center justify-between">
+        <div></div>
+        <span>&copy; 2025 Guillaume RICARD. All rights reserved.</span>
+        <div className="mr-5">
+          Language select:&nbsp;
+          <Select defaultValue="en">
+            <MenuItem value="en">English</MenuItem>
+            <MenuItem value="ja">日本語</MenuItem>
+          </Select>
+        </div>
+      </footer>
       <TanStackRouterDevtools />
-    </>
+    </Container>
   );
 }
