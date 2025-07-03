2a539283a2a28f38785d6a63858c3d4e9d764fd4
Guillaume RICARD <guillaume.ricard@mail.com>
2025-07-03 17:12:19 +0900

    feat: Add countdown widget once service starts
    Show minute and second count left before service time is over.

--- src/components/queue_view.tsx
+++ src/components/queue_view.tsx
@@ -5,10 +5,14 @@ import { useQueue } from "../client/hooks";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import { DateTime, Duration } from "luxon";
+
 import { hc } from "hono/client";
 import app from "../index.tsx";
 import { type WSMessageReceive } from "hono/ws";
 import {
+  SERVICE_TIME,
+  DEFAULT_PARTY_SIZE,
   type PrestoPostiMessage,
   type PrestoPostiTokenMessage,
   type PrestoPostiCheckInMessage,
@@ -18,7 +22,13 @@ let ws: WebSocket | null = null;
 
 export default function QueueView() {
   const [disabled, setDisabled] = useState(true);
+
+  const [partySize, setPartySize] = useState(DEFAULT_PARTY_SIZE);
+
+  const [countdownDate, setCountdownDate] = useState<DateTime>(DateTime.now());
+  const [countdownText, setCountdownText] = useState("");
   const [showCountdown, setShowCountdown] = useState(false);
+  const [countdownInterval, setCountdownInterval] = useState<NodeJS.Timeout>(0);
 
   const { token, deleteToken } = useQueue();
 
@@ -47,14 +57,40 @@ export default function QueueView() {
           const { partySize, eligible } = message;
           if (eligible) {
             setDisabled(false);
+            setPartySize(partySize);
           }
         } else if (message.type === "checkin") {
           setDisabled(true);
+          setCountdownDate(
+            DateTime.fromISO((message.checkInDate ?? "") as string) // serialization turns Date to ISO string
+              .plus({
+                milliseconds: partySize * SERVICE_TIME,
+              }),
+          );
           setShowCountdown(true);
         }
       } catch {}
     });
   }, [token]);
+
+  // Update countdown display
+  useEffect(() => {
+    if (countdownInterval) {
+      clearInterval(countdownInterval);
+    }
+
+    const updateText = () => {
+      const duration = countdownDate.diff(DateTime.now());
+      if (duration.toMillis() < 0) {
+        clearInterval(countdownInterval);
+        return;
+      }
+      setCountdownText(duration.toFormat("mm:ss"));
+    };
+
+    updateText();
+    setCountdownInterval(setInterval(updateText, 1000));
+  }, [countdownDate, showCountdown, partySize]);
 
   const handleClick = useCallback(() => {
     if (!ws) {
@@ -75,7 +111,11 @@ export default function QueueView() {
       <Link to="/" onClick={deleteToken} className="underline">
         Cancel and go back
       </Link>
-      {showCountdown ? <Typography>Service has started</Typography> : ""}
+      {showCountdown ? (
+        <Typography variant="h1">{countdownText}</Typography>
+      ) : (
+        ""
+      )}
       <Button
         variant="contained"
         color="success"
