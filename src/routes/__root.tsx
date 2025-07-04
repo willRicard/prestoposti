231f56a6b90919187b649d5aeced965f7ed3d755
Guillaume RICARD <guillaume.ricard@mail.com>
2025-07-04 09:16:36 +0900

    fix: Remove unused language selector
    Cleanup UI elements unused in initial release.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,8 +2,6 @@ import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 import Container from "@mui/material/Container";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -24,16 +22,9 @@ function RootComponent() {
       <div className="absolute inset-0 flex flex-col items-center justify-center gap-9">
         <Outlet />
       </div>
-      <footer className="py-3 absolute bottom-0 left-0 right-0 flex items-center justify-between">
+      <footer className="py-3 absolute bottom-0 left-0 right-0 flex items-center justify-center">
         <div></div>
         <span>&copy; 2025 Guillaume RICARD. All rights reserved.</span>
-        <div className="mr-5">
-          Language select:&nbsp;
-          <Select defaultValue="en">
-            <MenuItem value="en">English</MenuItem>
-            <MenuItem value="ja">日本語</MenuItem>
-          </Select>
-        </div>
       </footer>
       <TanStackRouterDevtools />
     </Container>
